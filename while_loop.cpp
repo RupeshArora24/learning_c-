4cebfc3553af2d90a7fc17cad6974905427206a3
Rupesh Arora <102988343+RupeshArora24@users.noreply.github.com>
2023-07-23 14:30:15 +0530

    Update while_loop.cpp
    while loop revision

--- while_loop.cpp
+++ while_loop.cpp
@@ -9,6 +9,6 @@ int main(){
     while(n>0){
        cout<<n<<endl;
        cin>>n;
-    }
+    } // While loop doesent run when n=0 as 0 represent to false and rest other numbers represent to true as 0 comes we come out of the while loop
     cout<<"The number entered is negetive."<<endl;
 }
